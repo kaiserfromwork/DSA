3dc3bd4bc35006f6a765debe31e0017ef53ad6ec
kaisefromwork <kaiserfromwork@gmail.com>
2025-03-05 19:05:34 -0300

    evaluate_postfix done

--- data_structures/exercises/evaluate_postfix.py
+++ data_structures/exercises/evaluate_postfix.py
@@ -14,7 +14,7 @@ postfix_expressions = [
     ("6 2 / 3 *", 9),  # (6 / 2) * 3 = 9
     ("10 2 8 * + 3 -", 23),  # (10 + (2 * 8)) - 3 = 23
     ("4 2 + 3 5 1 - * +", 18),  # (4 + 2) + (3 * (5 - 1)) = 19
-    ("7 2 3 + * 4 /", 8),  # (7 * (2 + 3)) / 4 = 8
+    ("7 2 3 + * 5 /", 7),  # (7 * (2 + 3)) / 5 = 7
 ]
 
 math_symbols = ['+', '-', '*', '/']
@@ -26,11 +26,11 @@ def eval_postfix(math_expression):
     result = []
     for char in math_expression:
         if char not in math_symbols:
-            print(f'{char} pushed to Stack')
+            # print(f'{char} pushed to Stack')
             numbers_stack.push(char)
         else:
             counter = 0
-            print(f'{char} is a math symbol')
+            # print(f'{char} is a math symbol')
             print(numbers_stack.pop())
             while counter < 2:
                 if numbers_stack.is_empty():
@@ -39,20 +39,20 @@ def eval_postfix(math_expression):
                     counter += 1
                     if counter < 2:
                         result.append(char)
-                        print(f"Added symbol to expression: {char}")
+                        # print(f"Added symbol to expression: {char}")
                         numbers_stack.pop()
                 else:
                     popped_item = numbers_stack.pop()
                     result.append(popped_item)
                     print(f"Added number to expression: {popped_item}")
 
-            print(f'Value of result: {result}')
-            a, b, c = result
-            to_eval = f'({c} {b} {a})'
-            print(f'Calculating expression: {to_eval}')
-            after_eval = eval(to_eval)
-            print(f'Result expression: {after_eval}')
-            numbers_stack.push(str(after_eval))
+            # print(f'Value of result: {result}')
+            a = result[::-1]
+            calc = "".join(a)
+            print(f'Calc: {calc}')
+            eval_calc = eval(calc)
+            # print(f'eval calc: {eval_calc}')
+            numbers_stack.push(str(eval_calc))
             result.clear()
 
     return numbers_stack.peek()
