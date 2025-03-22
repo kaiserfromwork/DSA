6a4a1df2823e2aba61eb4edc6e7714fdb6072952
kaisefromwork <kaiserfromwork@gmail.com>
2025-03-22 16:12:28 -0300

    implemented double linked list

--- data_structures/linked_list/double_linked_list.py
+++ data_structures/linked_list/double_linked_list.py
@@ -2,7 +2,6 @@
 
 class DoublyNode():
 
-
     def __init__(self, value, next=None, prev=None):
         self.value = value
         self.next = next
@@ -10,11 +9,10 @@ class DoublyNode():
 
 
     def __str__(self):
-        return f"{self.value, self.next, self.value}"
+        return str(self.value)
     
 
 class DoubleLinkedList():
-
 
     def __init__(self):
         self.head = None
@@ -22,9 +20,38 @@ class DoubleLinkedList():
 
     def append(self, value):
         if self.head is None:
-            self.head = DoubleLinkedList(value)
+            self.head = DoublyNode(value)
             return
         
         current = self.head
         while current.next:
-            pass
+            current = current.next
+        
+        current.next = DoublyNode(value, prev=current)
+
+
+    def display_list(self):
+        current = self.head
+        elements = []
+
+        while current:
+            elements.append(str(current.value))
+            current = current.next
+
+        print(" <-> ".join(elements))
+########################### TEST ###########################
+
+db_node = DoublyNode(5)
+print(f"Single Print First Node: {db_node}")
+
+db_list = DoubleLinkedList()
+
+db_list.append(db_node)
+db_list.display_list()
+
+db_list.append(DoublyNode(25))
+db_list.append(DoublyNode(35))
+db_list.append(DoublyNode(40))
+
+print("Adding more nodes!")
+db_list.display_list()
